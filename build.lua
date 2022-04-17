e52190412bb9671688e9ca14da2f240d5890d507
Marcel Fabian Krüger <tex@2krueger.de>
2022-04-17 02:49:28 +0200

    Add .gitignore

---
+++ build.lua
@@ -0,0 +1,6 @@
+module = "luamathalign"
+typesetexe = "lualatex"
+unpackfiles = {"*.dtx"}
+installfiles = {"*.sty", "*.lua"}
+sourcefiles = {"*.dtx", "*.lua"}
+-- unpackopts = "-interaction=batchmode"
