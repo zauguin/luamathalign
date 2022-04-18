46670e9ccc8f81ea854f453662cee1bd223cbd16
Marcel Fabian Krüger <tex@2krueger.de>
2022-04-18 16:55:16 +0200

    Prepare for release

--- build.lua
+++ build.lua
@@ -1,7 +1,21 @@
 module = "luamathalign"
 typesetexe = "lualatex"
+tdsroot = "lualatex"
 checkengines = {"luatex"}
 unpackfiles = {"*.dtx"}
 installfiles = {"*.sty", "*.lua"}
 sourcefiles = {"*.dtx", "*.lua"}
--- unpackopts = "-interaction=batchmode"
+
+uploadconfig = {
+  pkg = "luamathalign",
+  version = "v0.1",
+  author = "Marcel Krüger",
+  license = "lppl1.3c",
+  summary = "More flexible alignment in amsmath environments",
+  ctanPath = "/macros/luatex/latex/luamathalign`",
+  -- repository = "https://github.com/zauguin/luamathalign/"
+  description = "Allow aligning mathematical expressions on points where \z
+    where direcly using & is not possible, especially in nested macros or environments.",
+  topic = {'maths', 'luatex'},
+  -- update = true,
+}
