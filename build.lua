b21c31b9dcf254ec6a1c0b6eae585ab885140c92
Marcel Fabian Krüger <tex@2krueger.de>
2022-05-04 09:27:34 +0200

    Step version (again)

--- build.lua
+++ build.lua
@@ -8,7 +8,7 @@ sourcefiles = {"*.dtx", "*.lua"}
 
 uploadconfig = {
   pkg = "luamathalign",
-  version = "v0.2",
+  version = "v0.3",
   author = "Marcel Krüger",
   license = "lppl1.3c",
   summary = "More flexible alignment in amsmath environments",
